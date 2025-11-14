2be60c3d67ce78a21858d89bde4094b0e1c15e38
toctoce <toctoce@naver.com>
2025-11-14 21:55:20 +0900

    feat(Exception): ChessException, InvalidPositionException 생성

---
+++ src/main/java/chess/common/exception/ChessException.java
@@ -0,0 +1,7 @@
+package chess.common.exception;
+
+public class ChessException extends IllegalArgumentException {
+    public ChessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/chess/common/exception/InvalidPositionException.java
@@ -0,0 +1,7 @@
+package chess.common.exception;
+
+public class InvalidPositionException extends ChessException {
+    public InvalidPositionException(String message) {
+        super(message);
+    }
+}
