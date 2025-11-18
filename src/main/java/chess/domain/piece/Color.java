ffd47d279d4d436edaf4e5a72df619d29708ea52
toctoce <toctoce@naver.com>
2025-11-18 12:28:23 +0900

    feat(Color): Color enum에 기물 시작 위치, 방향 저장

--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -1,13 +1,35 @@
 package chess.domain.piece;
 
 public enum Color {
-    WHITE,
-    BLACK;
+    WHITE(1, 1, 0),
+    BLACK(-1, 6, 7);
+
+    private final int direction;
+    private final int pawnStartRank;
+    private final int pieceStartRank;
+
+    Color(int direction, int pawnStartRank, int pieceStartRank) {
+        this.direction = direction;
+        this.pawnStartRank = pawnStartRank;
+        this.pieceStartRank = pieceStartRank;
+    }
 
     public Color opposite() {
         if (this == WHITE) {
             return BLACK;
         }
         return WHITE;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+    
+    public int getPieceStartRank() {
+        return pieceStartRank;
+    }
+
+    public int getPawnStartRank() {
+        return pawnStartRank;
     }
 }
