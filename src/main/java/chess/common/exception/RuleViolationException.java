e243f2dd283b4d74bd5d33e8e71c3e173bd7559a
toctoce <toctoce@naver.com>
2025-11-19 16:55:07 +0900

    feat(Exception): 이동 관련 새로운 예외 생성
    - RuleViolationException 생성함.
    - IllegalMoveException 생성함.
    - ErrorMessage에 세 매시지 추가함.

---
+++ src/main/java/chess/common/exception/RuleViolationException.java
@@ -0,0 +1,7 @@
+package chess.common.exception;
+
+public class RuleViolationException extends ChessException {
+    public RuleViolationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/chess/common/message/ErrorMessage.java
+++ src/main/java/chess/common/message/ErrorMessage.java
@@ -9,7 +9,16 @@ public enum ErrorMessage {
     POSITION_INVALID_RANK("대수 기보 표기법의 두 번째 글자는 1~8 이어야 합니다.: %s", true),
 
     PIECE_INVALID_CREATION_ARGUMENTS("Piece 생성 인자는 null일 수 없습니다."),
-    PIECE_NOT_FOUND("기물이 존재하지 않습니다.");
+    PIECE_NOT_FOUND("기물이 존재하지 않습니다."),
+
+    RULE_WRONG_TURN_PIECE("자신의 턴에 맞는 기물만 움직일 수 있습니다."),
+    RULE_SAME_POSITION_MOVE("제자리로 이동할 수 없습니다."),
+    RULE_FRIENDLY_FIRE("아군 기물이 있는 위치로 이동할 수 없습니다."),
+    RULE_INVALID_PIECE_MOVE("해당 기물의 이동 규칙에 어긋납니다."),
+    RULE_PATH_BLOCKED("이동 경로에 장애물이 있어 이동할 수 없습니다."),
+
+    RULE_KING_IN_CHECK_AFTER_MOVE("킹이 체크 상태가 되는 위치로는 이동할 수 없습니다."),
+    ;
 
 
     private final String message;
