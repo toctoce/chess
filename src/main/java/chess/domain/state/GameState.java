dcc14f206ecf9f02d92df8b76704898026469f8d
toctoce <toctoce@naver.com>
2025-11-20 13:47:20 +0900

    feat(GameState): 게임 상태 인터페이스 구현

---
+++ src/main/java/chess/domain/state/GameState.java
@@ -0,0 +1,14 @@
+package chess.domain.state;
+
+import chess.domain.board.Board;
+import chess.domain.board.Position;
+import chess.domain.piece.Color;
+
+public interface GameState {
+
+    GameState move(Position from, Position to, Board board, Color turnColor);
+
+    boolean isFinished();
+
+    String status();
+}
