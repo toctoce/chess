d2c85a85a9decc3edf23a988a79d851d963a58cb
toctoce <toctoce@naver.com>
2025-11-21 00:06:23 +0900

    feat(CheckmateDetector): 체크메이트 검사 클래스 구현

---
+++ src/main/java/chess/domain/rule/CheckmateDetector.java
@@ -0,0 +1,19 @@
+package chess.domain.rule;
+
+import chess.domain.board.Board;
+import chess.domain.piece.Color;
+
+public class CheckmateDetector {
+
+    private final CheckDetector checkDetector;
+    private final RuleValidator ruleValidator;
+
+    public CheckmateDetector(CheckDetector checkDetector, RuleValidator ruleValidator) {
+        this.checkDetector = checkDetector;
+        this.ruleValidator = ruleValidator;
+    }
+
+    public boolean isCheckmate(Board board, Color currentColor) {
+        return checkDetector.isCheck(board, currentColor) && !ruleValidator.anyPieceHasLegalMove(board, currentColor);
+    }
+}
